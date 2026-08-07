8633f80be7a835ee13fa4a4642019e667b1398db
mrtnRitter <mrtn.ritter@googlemail.com>
2026-08-07 14:40:44 +0200

    Update tb_link_api - Helper for strings

--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -21,10 +21,11 @@ import datetime
 from dotenv import load_dotenv
 from pathlib import Path
 import json
+from typing import Literal
 import FlowAPI
 
 # --------- FUNC MAIN---------
-def tb_link_api(api):
+def tb_link_api(api: Literal["metadata", "ark"]):
     env_path = Path(__file__).parent / "cred.env"
     load_dotenv(env_path)
 
